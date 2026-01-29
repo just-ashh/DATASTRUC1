92435c47223d552805ac4d38641b72c7dec8b61c
just-ashh <angelclaire.bacsal@lorma.edu>
2026-01-29 17:29:38 +0800

    DATASTRUC push

--- activities/ArtistRegistration.java
+++ activities/ArtistRegistration.java
@@ -2,47 +2,59 @@ package activities;
 import java.util.*;
 
 public class ArtistRegistration{
+    public static Scanner input = new Scanner(System.in);
     public static void main(String[] args){
-        String strTagname, strArtType, strTools, strCommissionStatus;
 
+        String strTagname, strArtType, strTools, strCommissionStatus;
         int Experience;
+        int registrationCount = 0;
 
         System.out.println("""
                 Artist Registration!!!
                 (Free registration for Artist Commission Support!)
                 """);
-        strTagname = UserInput("Enter username: ");
-        strArtType = UserInput("Traditional or Digital?: ");
-        strTools = UserInput("Tools used: ");
-        Experience = ArtistExperience("How long have you been drawing (in years)?: ");
-        strCommissionStatus = UserInput("Commission status?: ");
 
-        ArtistRegistered(strTagname, strArtType, strTools, strCommissionStatus, Experience);
+        while(true){
+            strTagname = UserInput("Enter username: ");
+            strArtType = UserInput("Traditional or Digital?: ");
+            strTools = UserInput("Tools used: ");
+            Experience = ArtistExperience("How long have you been drawing (in years)?: ");
+            strCommissionStatus = UserInput("Commission status?: ");
+
+            System.out.println("Input another registration? (Y/N): ");
+            String choice = input.nextLine();
+
+            if(!choice.equalsIgnoreCase("Y")){
+                System.out.println("Registration closed. Thank you! <3");
+                System.out.println();
+                break;
+            }
+
+            registrationCount++;
+        }
+         System.out.println("==== Final Registration ====");
+         ArtistRegistered(strTagname, strArtType, strTools, strCommissionStatus, Experience);
+         System.out.println();
+         System.out.println("Total Registrations: " + registrationCount);
 
     }
-
     public static String UserInput(String strInput){
         String Info;
-        Scanner input = new Scanner(System.in);
         System.out.println(strInput);
         Info = input.nextLine();
         
         return Info;
-
     }
 
 
     public static int ArtistExperience(String userInput){
         int Time;
-        Scanner input = new Scanner(System.in);
         System.out.println(userInput);
-        Time = input.nextInt();
+        Time = input.nextInt(); input.nextLine();
 
         return Time;
-
     }
     
-
     public static void ArtistRegistered(String strTagname, String strArtType, String strTools, String strCommissionStatus, int Time){
         System.out.println();
         System.out.println("Username: " + strTagname);
@@ -50,9 +62,6 @@ public class ArtistRegistration{
         System.out.println("Tools used: " + strTools);
         System.out.println("Experience: " + Time + " years");
         System.out.println("Commission Status: " + strCommissionStatus);
-
     }
-
-
 
 }
